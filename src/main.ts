698e29c3e95b770b262ee41f089ca4bb3ac5c9f9
VariableSan <nursultan_nurgaliyev@proton.me>
2023-04-22 20:48:52 +0600

    Added lodash

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { ViteSSG } from "vite-ssg"
 import { setupLayouts } from "virtual:generated-layouts"
+import { ViteSSG } from "vite-ssg"
+import generatedRoutes from "~pages"
 import App from "./App.vue"
 import type { UserModule } from "./types"
-import generatedRoutes from "~pages"
 
-import "./styles/main.css"
 import "virtual:windi.css"
+import "./styles/main.css"
 
 const routes = setupLayouts(generatedRoutes)
 
